acd5bc26f40d838f979ffcb3de66e0b2eb0cbfaa
Alexey Danilov <danilov-av@1502.moscow>
2020-08-14 00:26:19 +0300

    fixing cons tab

--- src/scripts/cons_parse.js
+++ src/scripts/cons_parse.js
@@ -1,9 +1,11 @@
 function Cons_tab(id) {
-	$(".cons_tab").removeClass("cons_tab_active");
-	$("#constab_" + id).addClass("cons_tab_active");
-	$(".table_cons_container").css("display", "none");
-	$("#constable_" + id).css("display", "block");
-	animateCSS("constable_" + id, "fadeIn", () => {});
+	if(!$("#constab_" + id).hasClass("cons_tab_active")){
+		$(".cons_tab").removeClass("cons_tab_active");
+		$("#constab_" + id).addClass("cons_tab_active");
+		$(".table_cons_container").css("display", "none");
+		$("#constable_" + id).css("display", "block");
+		animateCSS("constable_" + id, "fadeIn", () => {});
+	}
 }
 
 $(document).ready(function () {
