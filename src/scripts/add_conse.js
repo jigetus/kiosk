082c4e96248e1f769e198d533d641a873d10c93d
Алексей Данилов <danilov-av@lyceum.local>
2020-09-14 14:46:19 +0300

    add consultation

--- src/scripts/add_conse.js
+++ src/scripts/add_conse.js
@@ -2,8 +2,12 @@ function Cons_tab(id) {
 	if (!$("#constab_" + id).hasClass("cons_tab_active")) {
 		$(".cons_tab").removeClass("cons_tab_active");
 		$("#constab_" + id).addClass("cons_tab_active");
-		$(".table_cons_container").css("display", "none");
-		$("#constable_" + id).css("display", "block");
-		animateCSS("constable_" + id, "fadeIn", () => {});
+		$(".images_cons").css("display", "none");
+		$("#cons_image_" + id).css("display", "block");
+		animateCSS("cons_image_" + id, "fadeIn", () => {});
 	}
 }
+
+$(document).ready(() => {
+	$(".images_cons").css("display", "none");
+});
