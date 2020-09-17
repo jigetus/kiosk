6e2560486abdfe7269c6c8365f83a11e07c62f13
Алексей Данилов <danilov-av@lyceum.local>
2020-09-17 13:33:19 +0300

    release 2020

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -18,12 +18,14 @@ class Router {
 		this.currentState = "menu";
 		this.prevState = null;
 		this.path = "/menu";
+		this.background.style.opacity == "100";
+		this.isBusy = false;
 	}
 
 	HideBackground() {
-		animateCSS(this.background.id, "fadeOut", () => {
-			this.background.style.opacity = "0";
-		});
+		// animateCSS(this.background.id, "fadeOut", () => {
+		// 	this.background.style.opacity = "0";
+		// });
 	}
 	ShowBackground() {
 		this.background.style.opacity = "100";
@@ -37,8 +39,13 @@ class Router {
 		const node = document.getElementById(tab_id);
 		node.style.display = "block";
 	}
+	isBackGroundShow() {
+		if (this.background.style.opacity !== "0") return true;
+		return false;
+	}
 	GetPrevState() {
-		const reg = new RegExp("(/[a-z]*)", "g");
+		// const reg = new RegExp("(/[a-z]*)", "g");
+		const reg = new RegExp("(/[a-z 0-9]*)", "g");
 		let rez = this.path.match(reg);
 		let back = rez[rez.length - 2];
 		const current = rez[rez.length - 1];
@@ -47,37 +54,51 @@ class Router {
 		this.prevState = back;
 	}
 	GoTo(tab_id) {
-		this.HideBackground();
-		this.path += "/" + tab_id;
-		animateCSS(this.currentState, "bounceOutLeft", () => {
-			this.HideTab(this.currentState);
-			this.ShowTab(tab_id);
-			animateCSS(tab_id, "bounceInRight", () => {
-				this.currentState = tab_id;
+		if (!this.isBusy) {
+			this.path += "/" + tab_id;
+			this.isBusy = true;
+			animateCSS(this.currentState, "bounceOutLeft", () => {
+				this.HideTab(this.currentState);
+				this.ShowTab(tab_id);
+				animateCSS(tab_id, "bounceInRight", () => {
+					this.currentState = tab_id;
+					this.isBusy = false;
+				});
 			});
-		});
+			console.log(this.path);
+		} else {
+			console.log("router is busy");
+		}
 	}
 	GoBack() {
-		this.GetPrevState();
-		if (this.prevState == "menu") {
-			this.ShowBackground();
-			animateCSS(this.currentState, "bounceOutRight", () => {
-				this.HideTab(this.currentState);
-				this.ShowTab(this.prevState);
-				animateCSS(this.prevState, "bounceInLeft", () => {
-					this.currentState = this.prevState;
-					this.prevState = null;
+		if (!this.isBusy) {
+			this.GetPrevState();
+			this.isBusy = true;
+			if (this.prevState == "menu") {
+				// this.ShowBackground();
+				animateCSS(this.currentState, "bounceOutRight", () => {
+					this.HideTab(this.currentState);
+					this.ShowTab(this.prevState);
+					animateCSS(this.prevState, "bounceInLeft", () => {
+						this.currentState = this.prevState;
+						this.prevState = null;
+						this.isBusy = false;
+					});
 				});
-			});
+			} else {
+				animateCSS(this.currentState, "bounceOutRight", () => {
+					this.HideTab(this.currentState);
+					this.ShowTab(this.prevState);
+					animateCSS(this.prevState, "bounceInLeft", () => {
+						this.currentState = this.prevState;
+						this.prevState = null;
+						this.isBusy = false;
+					});
+				});
+			}
+			console.log(this.path);
 		} else {
-			animateCSS(this.currentState, "bounceOutRight", () => {
-				this.HideTab(this.currentState);
-				this.ShowTab(this.prevState);
-				animateCSS(this.prevState, "bounceInLeft", () => {
-					this.currentState = this.prevState;
-					this.prevState = null;
-				});
-			});
+			console.log("router is busy");
 		}
 	}
 	GoToMenu() {
@@ -100,10 +121,17 @@ class Router {
 function SmartClick(targetSelector, action) {
 	$(targetSelector).click((ev) => {
 		ev.preventDefault();
-		action();
-		$(targetSelector).prop("disabled", true);
-		setTimeout(() => {
-			$(targetSelector).prop("disabled", false);
-		}, 1500);
+		if (!router.isBusy) {
+			action();
+		} else {
+			return false;
+		}
+		// action();
+		// $(targetSelector).prop("disabled", true);
+		// $(".back_action").prop("disabled", true);
+		// setTimeout(() => {
+		// 	$(targetSelector).prop("disabled", false);
+		// 	$(".back_action").prop("disabled", false);
+		// }, 1500);
 	});
 }
--- src/scripts/kruzhki.js
+++ src/scripts/kruzhki.js
@@ -14,32 +14,72 @@ fetch(
 )
 	.then((answ) => answ.json())
 	.then((res) => {
+		console.log(res);
 		res.forEach((element) => {
 			//Создание вкладки для каждого кружка
 			const newtab = document.createElement("div");
-			newtab.styl;
-			newtab.innerHTML = `<div class="tabs noselect faster" id="kruzhok_${element.ID}">
+			newtab.classList = "tabs noselect faster kruzhok_info";
+			newtab.id = `kruzhok${element.ID}`;
+			let raspisanie = "";
+			if (element.Понедельник != "") {
+				raspisanie += "Понедельник: " + element.Понедельник + "<br>";
+			}
+			if (element.Вторник != "") {
+				raspisanie += "Вторник: " + element.Вторник + "<br>";
+			}
+			if (element.Среда != "") {
+				raspisanie += "Среда: " + element.Среда + "<br>";
+			}
+			if (element.Четверг != "") {
+				raspisanie += "Четверг: " + element.Четверг + "<br>";
+			}
+			if (element.Пятница != "") {
+				raspisanie += "Пятница: " + element.Пятница + "<br>";
+			}
+			if (element.Суббота != "") {
+				raspisanie += "Суббота: " + element.Суббота + "<br>";
+			}
+
+			newtab.innerHTML = `
 			<div class="tab_title">
 			  <button class="back_action">
 				<img src="/images/close.svg" alt="" class="back_btn noselect" />
 			  </button>
-			  
 			</div>
-			<div class="class_containter scrollbar" id="zvonki_container">
-
+			<div class="class_containter kruzhok_info_container" id="kruzhok_container_${element.ID}">
+				<h1>${element.TITLE}</h1>
+				<h4>${element.CLASS} класс</h4>
+				<span>${element.DESC}</span>
+				<h2>Преподаватель: <br>${element.FIO}</h2>
+				<h3>Аудитория: ${element.KAB}</h3>
+				<h5>${raspisanie}</h5>
 			</div>
-		  </div>`;
-			document.getElementById("kruzhki_container").appendChild(newtab);
+		  `;
+			document.body.appendChild(newtab);
 
-			router.HideTab(`kruzhok_${element.ID}`);
-			ins.innerHTML += `<button class="card" id="card_btn_${element.ID}"> <h1>${
-				element.TITLE
-			}</h1><div class="topline" style="background: ${get_random_color()}"></div></button>`;
+			router.HideTab(`kruzhok${element.ID}`);
+			let isDistant = false;
+			if (element.KAB == "дистанционно") {
+				isDistant = true;
+			}
 
-			SmartClick(`#card_btn_${element.ID}`, () => {
-				router.GoTo(`kruzhok_${element.ID}`);
-				console.log("gotowork");
-			});
+			ins.innerHTML += `<button class="card" id="card_btn_${
+				element.ID
+			}" onClick="goToKruzhok(${element.ID})"> <h1>${element.TITLE}</h1>
+				<span><b>${element.CLASS}</b> <br> класс</span>
+				${isDistant ? `<div class="lenta">Дистанционно</div>` : ""}
+				
+				<div class="topline" style="background: ${get_random_color()}"></div></button>`;
 		});
 		SmartClick(".back_action", () => router.GoBack());
 	});
+
+const goToKruzhok = (id) => {
+	console.log("kruzhok_clicked");
+	if (!router.isBusy) {
+		console.log("kruzhok_action");
+		router.GoTo(`kruzhok${id}`);
+	} else {
+		return false;
+	}
+};
